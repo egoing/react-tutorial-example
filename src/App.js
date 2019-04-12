f79a6a9682bb4d2f82d719899e6a1a2072d73ef6
egoing <egoing@gmail.com>
2019-04-13 02:57:39 +0900

    11.2. 컴포넌트 만들기 2

--- src/App.js
+++ src/App.js
@@ -12,11 +12,38 @@ class Subject extends Component {
   }
 }
 
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">JavaScript</a></li>
+          </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return (
+      <article>
+          <h2>HTML</h2>
+          HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
