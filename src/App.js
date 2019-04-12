aa3fac48b854a11d626702451455679577077acb
egoing <egoing@gmail.com>
2019-04-13 02:42:56 +0900

    11. 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Subject extends Component {
+  render(){
+    return (
+      <header>
+          <h1>WEB</h1>
+          world wide web!
+      </header>  
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject></Subject>
       </div>
     );
   }
