0bc7e65ef73ce68255a840f5b848dc7b26834762
egoing <egoing@gmail.com>
2019-04-13 10:34:37 +0900

    12. props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Subject extends Component {
-  render(){
-    return (
-      <header>
-          <h1>WEB</h1>
-          world wide web!
-      </header>  
-    );
-  }
-}
+
 
 class TOC extends Component{
   render(){
@@ -30,9 +21,20 @@ class Content extends Component{
   render(){
     return (
       <article>
-          <h2>HTML</h2>
-          HTML is HyperText Markup Language.
+          <h2>{this.props.title}</h2>
+          {this.props.desc}
       </article>
+    );
+  }
+}
+
+class Subject extends Component {
+  render(){
+    return (
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>  
     );
   }
 }
@@ -41,9 +43,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
