430182626e2cd57f8c93ec71dcf0cc3ff6f773a8
egoing <egoing@gmail.com>
2019-04-14 14:58:00 +0900

    15.2. state를 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wid Web!'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
