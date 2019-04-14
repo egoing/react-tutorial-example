b82fbf87142365ce48d06dffc031be15d76a2fad
egoing <egoing@gmail.com>
2019-04-14 22:02:02 +0900

    17. 컴포넌트의 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -31,21 +31,14 @@ class App extends Component {
     }
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-            <h1><a href="/" onClick={function(e){
-              console.log(e);
-              e.preventDefault();
-              //this.state.mode = 'welcome';
-              this.setState({
-                mode:'welcome'
-              });
-            }.bind(this)}>{this.state.subject.title}</a></h1>
-            {this.state.subject.sub}
-        </header>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,7 +4,10 @@ class Subject extends Component {
       console.log('Subject render');
       return (
         <header>
-            <h1><a href="/">{this.props.title}</a></h1>
+            <h1><a href="/" onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage();
+            }.bind(this)}>{this.props.title}</a></h1>
             {this.props.sub}
         </header>  
       );
