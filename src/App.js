35430aef53b4d2e292ea5b5d7ab7a727d8a7c5f0
egoing <egoing@gmail.com>
2019-04-14 17:34:57 +0900

    16.3. 이벤트에서 state 변경하기

--- src/App.js
+++ src/App.js
@@ -39,7 +39,11 @@ class App extends Component {
             <h1><a href="/" onClick={function(e){
               console.log(e);
               e.preventDefault();
-            }}>{this.state.subject.title}</a></h1>
+              //this.state.mode = 'welcome';
+              this.setState({
+                mode:'welcome'
+              });
+            }.bind(this)}>{this.state.subject.title}</a></h1>
             {this.state.subject.sub}
         </header>
         <TOC data={this.state.contents}></TOC>
