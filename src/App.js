b4e77440dab235353983a623ba657447a16829c5
egoing <egoing@gmail.com>
2019-04-14 16:57:32 +0900

    16.2. 이벤트 설치

--- src/App.js
+++ src/App.js
@@ -31,10 +31,17 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+            <h1><a href="/" onClick={function(e){
+              console.log(e);
+              e.preventDefault();
+            }}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
