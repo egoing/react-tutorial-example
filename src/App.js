eaf6b2dca6b0d4119dfe576e9826928d6226055f
egoing <egoing@gmail.com>
2019-04-15 00:37:22 +0900

    17.3. 콤포넌트 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super(props);
     this.state = {
       mode:'read',
-      subject:{title:'WEB', sub:'World Wid Web!'},
+      selected_content_id:2,
+      subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
@@ -26,8 +27,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
@@ -40,8 +49,11 @@ class App extends Component {
         >
         </Subject>
         <TOC 
-          onChangePage={function(){
-            this.setState({mode:'read'});
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
           }.bind(this)} 
           data={this.state.contents}
         ></TOC>
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,9 +11,10 @@ class TOC extends Component{
           <li key={data[i].id}>
             <a 
               href={"/content/"+data[i].id}
+              data-id={data[i].id}
               onClick={function(e){
                 e.preventDefault();
-                this.props.onChangePage();
+                this.props.onChangePage(e.target.dataset.id);
               }.bind(this)}
             >{data[i].title}</a>
           </li>);
