74c5bd9337f5bf00497fb590dceaa5bc732d5e34
egoing <egoing@gmail.com>
2019-04-15 16:38:05 +0900

    19.2. mode 변경 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TOC from "./components/TOC";
 import Content from "./components/Content"
 import Subject from "./components/Subject"
+import Control from "./components/Control"
 import './App.css';
 
 
@@ -57,6 +58,11 @@ class App extends Component {
           }.bind(this)} 
           data={this.state.contents}
         ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
---
+++ src/components/Control.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+class Control extends Component {
+    render(){
+      console.log('Subject render');
+      return (
+        <ul>
+          <li><a href="/create" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('create');
+          }.bind(this)}>create</a></li>
+          <li><a href="/update" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('update');
+          }.bind(this)}>update</a></li>
+          <li><input  onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('delete');
+          }.bind(this)} type="button" value="delete"></input></li>
+        </ul>
+      );
+    }
+  }
+
+export default Control;
