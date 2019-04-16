d80bd49d3c0d3ad6570f95d4454cf3520107f0a7
egoing <egoing@gmail.com>
2019-04-16 15:36:09 +0900

    20.3. update 구현 - state 변경

--- src/App.js
+++ src/App.js
@@ -46,35 +46,33 @@ class App extends Component {
       _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else  if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
-        // add content to this.state.contents
         this.max_content_id = this.max_content_id+1;
-        // this.state.contents.push(
-        //   {id:this.max_content_id, title:_title, desc:_desc}
-        // );
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
-        console.log(_title, _desc);
       }.bind(this)}></CreateContent>
     } else  if(this.state.mode === 'update'){
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
-        // add content to this.state.contents
-        this.max_content_id = this.max_content_id+1;
-        // this.state.contents.push(
-        //   {id:this.max_content_id, title:_title, desc:_desc}
-        // );
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
-        this.setState({
-          contents:_contents
-        });
-        console.log(_title, _desc);
-      }.bind(this)}></UpdateContent>
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+        }.bind(this)}></UpdateContent>
     }
     return _article;
   }
--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -4,6 +4,7 @@ class UpdateContent extends Component{
     constructor(props){
       super(props);
       this.state = {
+        id:this.props.data.id,
         title:this.props.data.title,
         desc:this.props.data.desc
       }
@@ -22,11 +23,13 @@ class UpdateContent extends Component{
               onSubmit={function(e){
                 e.preventDefault();
                 this.props.onSubmit(
-                  e.target.title.value,
-                  e.target.desc.value
+                  this.state.id,
+                  this.state.title,
+                  this.state.desc
                 );
               }.bind(this)}
             >
+              <input type="hidden" name="id" value={this.state.id}></input>
               <p>
                 <input 
                   type="text" 
